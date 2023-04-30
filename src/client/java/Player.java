9985b79f56dc0306fdedb613823b17eef14966d0
Nambu <115127437+nAmBuCde@users.noreply.github.com>
2023-04-30 21:58:05 +0900

    Playerクラス
    Otelloクラスもちょっとやりました

--- src/client/java/Player.java
+++ src/client/java/Player.java
@@ -1,5 +1,22 @@
-package client.java;
-
 public class Player {
-    
+
+	private String myName = ""; //プレイヤ名
+	private String myColor = ""; //先手後手情報(白黒)
+
+	// メソッド
+	public void setName(String name){ // プレイヤ名を受付
+		myName = name;
+	}
+	
+	public String getName(){	// プレイヤ名を取得
+		return myName;
+	}
+	
+	public void setColor(String c){ // 先手後手情報の設定
+		myColor = c;
+	}
+	
+	public String getColor(){ // 先手後手情報の取得
+		return myColor;
+	}
 }
