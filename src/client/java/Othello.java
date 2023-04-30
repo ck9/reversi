2c465ac2723220ae20228425e00db3fa5f309726
ck9 <0926cookie@gmail.com>
2023-05-01 01:05:24 +0900

    画面遷移実装(タイトル/接続中/ゲーム)

--- src/client/java/Othello.java
+++ src/client/java/Othello.java
@@ -1,52 +1,77 @@
 package client.java;
 
 public class Othello {
-	private int row = 8;	//オセロ盤の縦横マス数(2の倍数のみ)
-	private String [] grids = new String [row * row]; //局面情報
-	private String turn; //手番
-	private String gamemode;
-	private String myColor = ""; //先手後手情報(白黒)
+	private int boardSize = 8; //盤面のサイズ
+    //              board = 0: 空白, 1: 黒, 2: 白
+	private int[][] board = new int[boardSize][boardSize]; //盤面の状態
+	private String turn; //手番 (black or white)
+	private String gameMode; //ゲームモード (easy or normal or hard or pvp)
 
-	// コンストラクタ
 	public Othello(){
-		turn = "black"; //黒が先手
-		for(int i = 0 ; i < row * row ; i++){
-			grids[i] = "board"; //初めは石が置かれていない
-			int center = row * row / 2;
-			grids[center - row / 2 - 1] = "black";
-			grids[center + row / 2    ] = "black";
-			grids[center - row / 2    ] = "white";
-			grids[center + row / 2 - 1] = "white";
-		}
+        // 諸々初期化
+		turn = "black";
+        gameMode = "easy";
 	}
 
-	// メソッド
+    public void startGameLocal(int difficulty) {
+        System.out.println("startGameLocal difficulty: " + difficulty);
+        switch (difficulty) {
+            case 0:
+                gameMode = "easy";
+                break;
+            case 1:
+                gameMode = "normal";
+                break;
+            case 2:
+                gameMode = "hard";
+                break;
+        }
+        System.out.println("startGameLocal gameMode: " + gameMode);
+        startGame();
+    }
+
+    public void startGameNetwork() {
+        gameMode = "pvp";
+        startGame();
+    }
+
+    private void startGame() {
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++){
+                board[i][j] = 0;
+            }
+        }
+        int center = boardSize / 2;
+        board[center - 1][center - 1] = 2;
+        board[center][center] = 2;
+        board[center - 1][center] = 1;
+        board[center][center - 1] = 1;
+
+        turn = "black";
+    }
+
+    public String getGameMode() {
+        System.out.println("getGameMode: " + gameMode);
+        return gameMode;
+    }
+
 	public String checkWinner(){	// 勝敗を判断
-		int BlackWin = 0;
-		for(int i = 0 ; i < row * row ; i++){
-			switch(grids[i]){
-				case "black":
-					BlackWin++;
-					break;
-				case "white":
-					BlackWin--;
-					break;
-			}
-		}
-		if(BlackWin>0)
-			return "black";
-		else if(BlackWin<0)
-			return "white";
-		else
-			return "draw";
-		
+
+		return "";
 	}
+
 	public String getTurn(){ // 手番情報を取得
 		return turn;
 	}
-	public String [] getGrids(){ // 局面情報を取得
-		return grids;
+
+	public int getBoardSize(){ // 盤面のサイズを取得
+		return boardSize;
 	}
+
+    public int getBoard(int x, int y) {
+        return board[x][y];
+    }
+
 	public void changeTurn(){ // 手番を変更
 		switch(turn) {
 		case "black":
@@ -57,13 +82,13 @@ public class Othello {
 			break;
 		}
 	}
+
 	public boolean isGameover(){	// 対局終了を判断
 		return true;
 	}
+
 	public boolean putStone(int i, String color, boolean effect_on){ // (操作を)局面に反映
 		return true;
 	}
-	public int getRow(){ //縦横のマス数を取得
-		return row;
-	}
+
 }
