a6289796a5d709f0b8f8365e9ee593110c17fc83
Yuki K <kobiyama-yuki-vn@ynu.jp>
2023-05-08 20:12:19 +0900

    add other methods

--- src/client/java/Othello.java
+++ src/client/java/Othello.java
@@ -3,6 +3,7 @@ package client.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Othello {
     private int row = 8; //盤面のサイズ
@@ -74,99 +75,165 @@ public class Othello {
         current_turn = "black";
     }
 
-    // 黒白の差, 壁際の石の数の差, 可能手数の差で評価値を計算（黒が有利なら正の値）
-    // ゲーム終了時の評価値が無限大、または無限小になるように処理を追加予定
+    // ボードの差, 確定石の差, 可能手数の差で評価値を計算（黒が有利なら正の値）
     public int cal_evaluation_value(int[][] board) {
-        int result_value = 0, cnt_value = 0, bythewall_value = 0, possible_moves_value = 0;
-        int cnt_weight = 1, bythewall_weight = 1, possible_moves_weight = 1;
-        int corner_point = 5, side_point = 3;
-
-        for (int y = 0; y < board.length; ++y) {
-        for (int x = 0; x < board[y].length; ++x) {
-            if (board[y][x] == 1) 
-            cnt_value++; 
-            else if (board[y][x] == 0)
-            cnt_value--;
-
-            if ((y == 0 && x == 0) || (y == 0 && x == row - 1) ||(y == row - 1 && x == 0) ||(y == row - 1 && x == row - 1)) {
-            if (board[y][x] == 1) 
-                bythewall_value += corner_point; 
-            else if (board[y][x] == 0) 
-                bythewall_value -= corner_point;
-            } 
-            else if (y == 0 || x == 0 || y == row - 1 || x == row - 1) {
-            if (board[y][x] == 1)
-                bythewall_value += side_point; 
-            else if (board[y][x] == 0) 
-                bythewall_value -= side_point;
+        if(is_end_state(board)){
+            int[] countStones = get_countStones(board);
+            if(countStones[0] > countStones[1]){
+                return Integer.MAX_VALUE;
+            }else if(countStones[0] < countStones[1]){
+                return Integer.MIN_VALUE;
+            }else{
+                return 0;
             }
         }
-        }
 
+        Random random = new Random();
+        double rnd = random.nextDouble();
+        int board_value = 0;
+        int[][] evaluationBoard = {
+          {100, -40, 20,  5,  5, 20, -40, 100},
+          {-40, -80, -1, -1, -1, -1, -80, -40},
+          { 20,  -1,  5,  1,  1,  5,  -1,  20},
+          {  5,  -1,  1,  0,  0,  1,  -1,   5},
+          {  5,  -1,  1,  0,  0,  1,  -1,   5},
+          { 20,  -1,  5,  1,  1,  5,  -1,  20},
+          {-40, -80, -1, -1, -1, -1, -80, -40},
+          {100, -40, 20,  5,  5, 20, -40, 100}
+        };
+
+        for (int y = 0; y < board.length; ++y) {
+          for (int x = 0; x < board[y].length; ++x) {
+            if (board[y][x] == 1){
+              board_value += evaluationBoard[y][x]; 
+            }
+            else if (board[y][x] == 0){
+              board_value -= evaluationBoard[y][x];
+            }
+          }
+        }
+        board_value *= (int)3 * rnd;
+
+        //fsの実装
+        int fs_value = 0;
+        int black_fs = 0, white_fs = 0;
+        List<Position> corners = new ArrayList<>(Arrays.asList(new Position(0, 0), new Position(row - 1,0), new Position(0, row - 1), new Position(row - 1, row - 1)));
+        List<Position> dzs = new ArrayList<>(Arrays.asList(new Position(-1, 0),new Position(0, -1),new Position(0, 1), new Position(1, 0)));
+        boolean[][] checked = new boolean[row][row];
+        for(Position corner:corners){
+          if(board[corner.y][corner.x] == 1){
+            black_fs++;
+            for(Position dz: dzs){
+              int dy = corner.y + dz.y, dx = corner.x + dz.x;
+              while(is_inrange(dy, dx) && board[dy][dx] == 1 && !checked[dy][dx]){
+                checked[dy][dx] = true;
+                black_fs++;
+                dy += dz.y;
+                dx += dz.x;
+              }
+            }
+          }
+          else if(board[corner.y][corner.x] == 0){
+            white_fs++;
+            for(Position dz: dzs){
+              int dy = corner.y + dz.y, dx = corner.x + dz.x;
+              while(is_inrange(dy, dx) && board[dy][dx] == 0 && !checked[dy][dx]){
+                checked[dy][dx] = true;
+                white_fs++;
+                dy += dz.y;
+                dx += dz.x;
+              }
+            }
+          }
+        }
+        rnd = random.nextDouble();
+        fs_value = (black_fs - white_fs) + (int)rnd * 33;
+
+        int possible_moves_value = 0;
         ArrayList<Position> possible_moves = get_possible_moves(board, "black");
         possible_moves_value += possible_moves.size();
         possible_moves = get_possible_moves(board, "white");
         possible_moves_value -= possible_moves.size();
+        rnd = random.nextDouble();
+        possible_moves_value = (possible_moves_value + (int)rnd * 2) * 10;
 
-        result_value = cnt_value * cnt_weight + bythewall_value * bythewall_weight + possible_moves_value * possible_moves_weight;
+        int result_value = 0;
+        int fs_weight = 5, board_weight = 2, possible_moves_weight = 1;
+        result_value = board_value * board_weight + fs_value * fs_weight + possible_moves_value * possible_moves_weight;
 
         return result_value;
+    }
+
+    public int[] get_countStones(int[][] board){
+      // 0: black, 1: white
+      int[] countStones = new int[2];
+      for(int i = 0; i < row; i++){
+        for(int j = 0; j < row; j++){
+          if(board[i][j] == 1)
+            countStones[0]++;
+          else if(board[i][j] == 0){
+            countStones[1]++;
+          }
+        }
+      }
+      return countStones;
     }
 
     public int minimax(int[][] board, int depth, int alpha, int beta, String turn) {
         ArrayList<Position> possible_moves = get_possible_moves(board, turn);
         if (depth <= 0 || possible_moves.isEmpty())
-        return cal_evaluation_value(board);
+          return cal_evaluation_value(board);
 
         int val = 0;    
         if (turn.equals("black")) {
-        val = Integer.MIN_VALUE;
-        for (Position move : possible_moves) {
-            int[][] copiedboard = new int[row][];
-            for (int i = 0; i < row; ++i)
-            copiedboard[i] = board[i].clone();
+          val = Integer.MIN_VALUE;
+          for (Position move : possible_moves) {
+              int[][] copiedboard = new int[row][];
+              for (int i = 0; i < row; ++i)
+                copiedboard[i] = board[i].clone();
             
-            make_move(copiedboard, move, turn);
-            int new_val = minimax(copiedboard, depth - 1, alpha, beta, "white");
-            val = Math.max(val, new_val);
-            alpha = Math.max(alpha, val);
+              make_move(copiedboard, move, turn);
+              int new_val = minimax(copiedboard, depth - 1, alpha, beta, "white");
+              val = Math.max(val, new_val);
+              alpha = Math.max(alpha, val);
 
-            if (alpha >= beta) break;
+              if (alpha >= beta) break;
         }
         } 
         else if (turn.equals("white")) {
-        val = Integer.MAX_VALUE;
-        for (Position move : possible_moves) {
-            int[][] copiedboard = new int[row][];
-            for (int i = 0; i < board.length; ++i)
-            copiedboard[i] = board[i].clone();
+          val = Integer.MAX_VALUE;
+          for (Position move : possible_moves) {
+              int[][] copiedboard = new int[row][];
+              for (int i = 0; i < board.length; ++i)
+                copiedboard[i] = board[i].clone();
 
-            make_move(copiedboard, move, turn);
-            int new_val = minimax(copiedboard, depth - 1, alpha, beta, "black");
-            val = Math.min(val, new_val);
-            beta = Math.min(beta, val);
+              make_move(copiedboard, move, turn);
+              int new_val = minimax(copiedboard, depth - 1, alpha, beta, "black");
+              val = Math.min(val, new_val);
+              beta = Math.min(beta, val);
 
-            if (alpha >= beta) break;
-        }
+              if (alpha >= beta) break;
+          }
         }
 
         return val;
     }
 
     public Position get_computer_move(int[][] board, String turn, String game_mode) {
-        int val = Integer.MIN_VALUE;
-        Position next_move = new Position(0, 0);
+        Position next_move = new Position(-1, -1);
         ArrayList<Position> possible_moves = get_possible_moves(board, turn);
-        // 可能手がない場合は(0, 0)を返す
-        if (possible_moves.isEmpty()) 
-        return next_move;
+
+        // 可能手がない場合は(-1, -1)を返す
+        if (possible_moves.isEmpty())
+          return next_move;
 
         int depth = 0;
         if (game_mode.equals("normal")) 
-        depth = 5; 
+          depth = 5; 
         else if (game_mode.equals("hard") || game_mode.equals("easy")) 
-        depth = 10;
+          depth = 7;
 
+        int val = (game_mode.equals("easy") && turn.equals("white")) || (!game_mode.equals("easy") && turn.equals("black")) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
         for (Position move : possible_moves) {
         int[][] copiedboard = new int[row][];
         for (int i = 0; i < row; ++i)
@@ -257,6 +324,7 @@ public class Othello {
     }
 
     public void make_move(int[][] board, Position move, String turn) {
+        if(move.equals(new Position(-1, -1))) return;
         List<Position> dzs = new ArrayList<>(
         Arrays.asList(
             new Position(-1, -1),
@@ -304,7 +372,8 @@ public class Othello {
             while (flip_length-- > 0) {
                 dy -= dz.y;
                 dx -= dz.x;
-                board[dy][dx] = 0;
+                if(is_inrange(dy, dx))
+                  board[dy][dx] = 0;
             }
             }
         }
@@ -313,6 +382,15 @@ public class Othello {
 
     public boolean is_inrange(int y, int x) {
         return 0 <= y && y < row && 0 <= x && x < row;
+    }
+
+    public boolean is_pass_state(int[][] board, String turn) {
+        boolean flag = false;
+        ArrayList<Position> possible_moves = get_possible_moves(board, turn);
+        if (possible_moves.isEmpty()) 
+          flag = true;
+
+        return flag;
     }
 
     public static void main(String[] args) {
@@ -348,7 +426,7 @@ public class Othello {
 
         // minimaxテスト
         int depth = 5;
-        int minimaxValue = othello.minimax(othello.current_board,depth,Integer.MIN_VALUE,Integer.MAX_VALUE,"black");
+        int minimaxValue = othello.minimax(othello.current_board,depth,Integer.MIN_VALUE,Integer.MAX_VALUE,"white");
         System.out.println("Minimax value: " + minimaxValue);
 
         // フリップ個数が多いテスト
@@ -370,6 +448,11 @@ public class Othello {
         othello.make_move(test_board, test_move, "white");
         System.out.println("Board after making a move:");
         printBoard(test_board);
+
+        // get_countStoneテスト
+        int[] stoneCounts = othello.get_countStones(test_board);
+        System.out.println("Black stones: " + stoneCounts[0]);
+        System.out.println("White stones: " + stoneCounts[1]);
     }
 
     // test用
