af86a4325d942ca68d0143f1f5ed005b74283c36
Yuki K <kobiyama-yuki-vn@ynu.jp>
2023-05-16 15:39:49 +0900

    unchange Client.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -62,6 +62,22 @@ public class Client extends JFrame{
     public void switchPanel(String panelName) {
         cardLayout.show(contentPane, panelName);
     }
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            try {
+                int port = Integer.parseInt(args[0]);
+                if (port > 0 && port < 65536) {
+                    new Client(port);
+                    return;
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "ポート番号は1~65535の整数で指定してください。", "エラー", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+        }
+        new Client(8888);
+        return;
+    }
 }
 
 class TitlePanel extends JPanel {
