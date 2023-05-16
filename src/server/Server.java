ecd05477299f229e8e170ddbc3f9897d41f1ed6a
Yuki K <kobiyama-yuki-vn@ynu.jp>
2023-05-16 15:36:04 +0900

    unchange Server.java

--- src/server/Server.java
+++ src/server/Server.java
@@ -118,4 +118,22 @@ public class Server{
 			System.out.println(e.getMessage());
 		}
 	}
+	public static void main(String[] args){
+		Integer port = 8888;
+		if (args.length > 0) {
+			try {
+				int argPort = Integer.parseInt(args[0]);
+				if (argPort > 0 && argPort < 65536) {
+					port = argPort;
+					return;
+				}
+			} catch (NumberFormatException e) {
+				return;
+			}
+		}
+		Server server = new Server(port); //サーバオブジェクトを用意
+		server.acceptClient(); //クライアント受け入れを開始
+		return;
+	}
+
 }
