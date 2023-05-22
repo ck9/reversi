b12ce4f8d579ab32b3df119e50931ce86b6c41a9
ck9 <0926cookie@gmail.com>
2023-05-22 20:32:59 +0900

    ポート指定周りの表記を統一

--- src/client/Client.java
+++ src/client/Client.java
@@ -64,19 +64,19 @@ public class Client extends JFrame{
     }
 
     public static void main(String[] args) {
+        Integer port = 8888;
         if (args.length > 0) {
             try {
-                int port = Integer.parseInt(args[0]);
-                if (port > 0 && port < 65536) {
-                    new Client(port);
-                    return;
+                Integer argPort = Integer.parseInt(args[0]);
+                if (argPort > 0 && argPort < 65536) {
+                    port = argPort;
                 }
             } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(null, "ポート番号は1~65535の整数で指定してください。", "エラー", JOptionPane.ERROR_MESSAGE);
                 return;
             }
         }
-        new Client(8888);
+        new Client(port);
         return;
     }
 }
